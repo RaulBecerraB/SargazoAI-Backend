88307976e681b30705fddc4761901f428cd17d91
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-11-15 01:40:43 -0600

    fix(CoordinateDTO): Remove 2 unused attributes

--- DTOs/CoordinateDTO.cs
+++ DTOs/CoordinateDTO.cs
@@ -5,6 +5,4 @@ public class CoordinateDTO
 
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    public double Speed_m_s { get; set; }
-    public double Direction_rad { get; set; }
 }
